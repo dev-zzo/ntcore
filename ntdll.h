7e29d0469d15e2fdefe09ebc7333add120325241
dev_zzo <gauri@tut.by>
2016-01-19 15:47:01 +0100

    removed STATUS_NO_MEMORY (defined in winnt.h)

--- ntdll.h
+++ ntdll.h
@@ -25,7 +25,6 @@
 #define STATUS_SUCCESS                      ((NTSTATUS)0x00000000L)
 #define STATUS_ALERTED                      ((NTSTATUS)0x00000101L)
 #define STATUS_INFO_LENGTH_MISMATCH         ((NTSTATUS)0xC0000004L)
-#define STATUS_NO_MEMORY                    ((NTSTATUS)0xC0000017L)
 #define STATUS_INVALID_BUFFER_SIZE          ((NTSTATUS)0xC0000206L)
 #endif
 
