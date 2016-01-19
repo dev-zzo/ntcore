aa4b779a30c847c2ea8b31e2efb27c0d55632fb0
dev_zzo <gauri@tut.by>
2016-01-19 12:57:06 +0100

    added STATUS_SUCCESS

--- ntdll.h
+++ ntdll.h
@@ -22,6 +22,7 @@
 #endif
 
 #ifndef STATUS_ALERTED
+#define STATUS_SUCCESS                      ((NTSTATUS)0x00000000L)
 #define STATUS_ALERTED                      ((NTSTATUS)0x00000101L)
 #define STATUS_INFO_LENGTH_MISMATCH         ((NTSTATUS)0xC0000004L)
 #endif
